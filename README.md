004619b49f7c554b47fdb1621cf1145c21ba8e6c
Olayanju Oluwadamilare Samuel <hadreytechsolutions@gmail.com>
2023-08-25 20:24:41 +0100

    Challenge Completed

---
+++ README.md
@@ -0,0 +1,18 @@
+ 1. *Title*
+ Week Two Code_Challenge
+
+2. *Description*
+ # Challenge 1: Student Grade Generator (Toy Problem)
+ This program writes the prompts the user to inpute the student marks. This program returns the correct grade
+
+ # Challenge 2: Speed Detector (Toy Problem)
+This program takes as input the speed of a car and return drivers demerit point and total number of demerit points
+
+ # Challenge 3: Net Salary Calculator (Toy Problem)
+ This program major task is to calculate an individual's Net Salary by getting inputs of basic salary ans benefits.This program retruns NHIFDeductions, gross salary, and net salary
+
+ 3. *Project Setup*
+These functions can be added to any program and gives good result whenever call in the program
+
+ 4. *Author & License*
+ This is licenced under the MIT
--- index.js
+++ index.js
@@ -37,3 +37,75 @@ console.log("Bye")
 speedDetector(150);
 
 
+//Challenge 3: Net Salary Calculator (Toy Problem)
+
+function calculateTaxableIncome(grossSalary, benefits) {
+    return grossSalary + benefits;
+}
+
+const payee = function calculatePayee(taxableIncome) {
+    if (taxableIncome <= 24000) {
+        return 0;
+    } else if (taxableIncome <= 32333) {
+        return (taxableIncome - 24000) * 0.1;
+    } else if (taxableIncome <= 40385) {
+        return 833.33 + (taxableIncome - 32333) * 0.15;
+    } else if (taxableIncome <= 57585) {
+        return 2083.33 + (taxableIncome - 40385) * 0.2;
+    } else if (taxableIncome <= 70833) {
+        return 4166.67 + (taxableIncome - 57585) * 0.25;
+    } else {
+        return 6250 + (taxableIncome - 70833) * 0.3;
+    }
+}
+
+const nhifDeductions = function calculateNHIFDeductions(grossSalary) {
+    if (grossSalary <= 5999) {
+        return 150;
+    } else if (grossSalary <= 7999) {
+        return 300;
+    } else if (grossSalary <= 11999) {
+        return 400;
+    } else if (grossSalary <= 14999) {
+        return 500;
+    } else if (grossSalary <= 19999) {
+        return 600;
+    } else if (grossSalary <= 24999) {
+        return 750;
+    } else if (grossSalary <= 29999) {
+        return 850;
+    } else if (grossSalary <= 34999) {
+        return 900;
+    } else if (grossSalary <= 39999) {
+        return 950;
+    } else {
+        return 1000;
+    }
+}
+
+const nssfDeductions = function calculateNSSF(grossSalary) {
+    return Math.min(grossSalary * 0.06, 2000);
+}
+
+
+function main() {
+    const basicSalary = parseFloat(prompt("Enter your basic salary: "));
+    const benefits = parseFloat(prompt("Enter the value of benefits: "));
+
+    const grossSalary = basicSalary + benefits;
+    const taxableIncome = calculateTaxableIncome(grossSalary, benefits);
+    const payee = calculatePayee(taxableIncome);
+    const nhifDeductions = calculateNHIFDeductions(grossSalary);
+    const nssfDeductions = calculateNSSF(grossSalary);
+
+    const netSalary = grossSalary - payee - nhifDeductions - nssfDeductions;
+
+    console.log("\nSalary Breakdown:");
+    console.log(`Gross Salary: ${grossSalary}`);
+    console.log(`Tax (PAYE): ${payee}`);
+    console.log(`NHIF Deductions: ${nhifDeductions}`);
+    console.log(`NSSF Deductions: ${nssfDeductions}`);
+    console.log(`Net Salary: ${netSalary}`);
+}
+
+main();
